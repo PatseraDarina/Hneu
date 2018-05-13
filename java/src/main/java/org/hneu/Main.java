7e38c726786619b48beef3738c0817e8c20bac46
Daryna Patsera <Daryna_Patsera@epam.com>
2018-05-13 13:35:34 +0300

    init commit22

--- java/src/main/java/org/hneu/Main.java
+++ java/src/main/java/org/hneu/Main.java
@@ -3,23 +3,67 @@ package org.hneu;
 
 import com.mongodb.hadoop.MongoInputFormat;
 import com.mongodb.spark.MongoSpark;
-import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 import org.bson.BSONObject;
-import org.bson.Document;
+import org.jfree.chart.ChartFactory;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.xy.DefaultXYDataset;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.ui.ApplicationFrame;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class Main {
+public class Main extends ApplicationFrame {
+
+    public Main(String title) {
+        super(title);
+    }
+
+    private static void fill(List<DimManufacturer> manufacturers) {
+        for (int i =0; i < 100_000; i++) {
+            DimManufacturer dimManufacturer = new DimManufacturer();
+            dimManufacturer.setManufacturer("Х/з \"Салтівський\" " + i);
+            dimManufacturer.setDimManufacturer(i);
+            manufacturers.add(dimManufacturer);
+        }
+    }
+
+    private static XYDataset createDataSet(JavaRDD<Row> filterRdd) {
+        DefaultXYDataset ds = new DefaultXYDataset();
+
+        ChartFactory.createTimeSeriesChart("TimeSeries title", "AxisLabel",
+                "valueAxis", new DefaultXYDataset(), true, true, true);
+
+        TimeSeries timeSeries = new TimeSeries("Jan-Dec2017");
+        List<Row> filterList = filterRdd.collect();
+        int j = 0;
+        for (Row row : filterList) {
+            double[][] series = new double[2][13];
+            List<String> items = row.getList(0);
+            for (int i = 1; i < 12; i++) {
+                //timeSeries.add(new Month(i, Integer.parseInt(items.get(0))), new Double(items.get(i)));
+                series[0][i] = (double)i;
+                series[1][i] = new Double(items.get(i));
+            }
+            ds.addSeries("Series-" + j, series);
+            j = j + 1;
+        }
+        return ds;
+    }
+
 
         public static void main(String[] args) {
+
+            System.setProperty("hadoop.home.dir", "D:\\ХНЕУ\\winutil");
+
             /* Create the SparkSession.
              * If config arguments are passed from the command line using --conf,
              * parse args for the values to set.
@@ -44,7 +88,7 @@ public class Main {
             // MongoDB connection string naming a collection to use.
             // If using BSON, use "mapred.input.dir" to configure the directory
             // where BSON files are located instead.
-            mongodbConfig.set("spark.mongodb.input.uri", "mongodb://127.0.0.1/hleb.hlebCollect");
+            mongodbConfig.set("spark.mongodb.input.uri", "mongodb://127.0.0.1/hleb.manufacturers");
 
             // Create an RDD backed by the MongoDB collection.
             JavaPairRDD<Object, BSONObject> documents = jsc.newAPIHadoopRDD(
@@ -54,29 +98,42 @@ public class Main {
                     BSONObject.class          // Value class
             );
 
-            DimManufacturer dimManufacturer = new DimManufacturer();
-            dimManufacturer.setManufacturer("Х/з \"Салтівський\" ");
-
             List<DimManufacturer> manufacturers = new LinkedList<>();
-            manufacturers.add(dimManufacturer);
+
+            //fill(manufacturers);
 
             JavaRDD<DimManufacturer> manufacturerJavaRDD = jsc.parallelize(manufacturers);
 
             SQLContext sqlContext = new SQLContext(jsc);
-
-//            spark-submit —driver-library-path /home/ubuntu/hadoop-2.9.0/lib/native —class org.hneu.Main ./target/hneu-1.0-SNAPSHOT.jar
-
+//
+////            spark-submit —driver-library-path /home/ubuntu/hadoop-2.9.0/lib/native —class org.hneu.Main ./target/hneu-1.0-SNAPSHOT.jar
+//
             Dataset dataset = sqlContext.createDataFrame(manufacturerJavaRDD, DimManufacturer.class);
-            dataset.createOrReplaceTempView("manufacturer");
+            dataset.createOrReplaceTempView("manufacturers");
+//
+            //MongoSpark.save(dataset);
 
-            MongoSpark.save(dataset);
+            //JavaMongoRDD<Document> rdd = MongoSpark.load(jsc);
+            Dataset<Row> dfc = MongoSpark.load(jsc).toDF();
+            JavaRDD<Row> rdd = dfc.javaRDD();
+            JavaRDD<Row> filterRdd = rdd.filter(row -> {
+                if ("7".equals(row.getList(1).get(0).toString()))
+                    return true;
+                return false;
+            });
 
-           // JavaMongoRDD<Document> rdd = MongoSpark.load(jsc);
+            System.out.println(filterRdd.count());
 
-            //JavaRDD<Document> filterRDD = rdd.filter(document -> document.get("hlebCollect", Hleb.class).getKodTovaru() == 1);
-            //System.out.println(filterRDD.count());
+            //JavaMongoRDD<Document> filterRdd = rdd.withPipeline(singletonList(Document.parse("{ $match: { dimManufacturer : 99998 } }")));
 
-            jsc.close();
+//            JFreeChart chart = ChartFactory.createTimeSeriesChaSrt("TimeSeries title", "AxisLabel",
+//                    "valueAxis", new DefaultXYDataset(), true, true, true);
+//            ChartPanel chartPanel = new ChartPanel(chart);
+//            chartPanel.setPreferredSize(new Dimension(560, 370));
+//            chartPanel.setMouseZoomable(true, false);
+//            JFrame jFrame = new JFrame();
+//            jFrame.setContentPane(chartPanel);
+//            jFrame.setVisible(true);
         }
 }
 
